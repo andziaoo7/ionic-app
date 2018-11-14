517b4fa5cbcda0628edf39e0d1a390260e8623fb
andziaoo7 <a.kalamacka@gmail.com>
2018-11-14 19:23:16 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://dev.vozilla.pl/api-client-portal/',
+  apiUrl: '',
   googleApiUrl: '',
-  googleApiKey: 'AIzaSyAW4v3vNQ-gqe24xHJabm6DE7Z1l9SXyPw',
+  googleApiKey: '',
 };
